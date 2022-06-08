61d3f577f72df115ad95c767e41b20039e7f57b6
“zhypower1997” <854091383@qq.com>
2022-06-08 12:13:41 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
-const withLess = require("next-with-less");
-module.exports = withLess({
-  lessLoaderOptions: {
-    lessOptions: {
-      modifyVars: {
-        "@primary-color": "#f74a49",
-        "@border-radius-base": ".5em"
-      }
-    }
-  }
-});
+module.exports = {
+}
